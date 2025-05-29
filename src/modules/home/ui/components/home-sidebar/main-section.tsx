e0cbabdea9e621f6dd9d26b40cd43fb1313caa8d
Abhijit Gobe <abhijitgobe00@gmail.com>
2025-05-29 13:32:03 +0530

    setup completed

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -1,39 +1,39 @@
 "use client";
-import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+// import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 import {
   SidebarContent,
   SidebarGroup,
   SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
+  // SidebarMenuButton,
+  // SidebarMenuItem
 } from "@/components/ui/sidebar";
 
 
 
-const items = [
-  {
-    title: "Home",
-    url: "/",
-    icon: HomeIcon
-  },
-  {
-    title: "Subscriptions",
-    url: "/feed/subscriptions",
-    icon: PlaySquareIcon,
-    auth: true
-  },
-  {
-    title: "trending",
-    url: "/feed/trending",
-    icon: FlameIcon
-  }
-]
+// const items = [
+//   {
+//     title: "Home",
+//     url: "/",
+//     icon: HomeIcon
+//   },
+//   {
+//     title: "Subscriptions",
+//     url: "/feed/subscriptions",
+//     icon: PlaySquareIcon,
+//     auth: true
+//   },
+//   {
+//     title: "trending",
+//     url: "/feed/trending",
+//     icon: FlameIcon
+//   }
+// ]
 export const MainSection = () => {
   return (
     <SidebarGroup>
       <SidebarContent>
         <SidebarMenu>
-
+          <h1>Hello</h1>
         </SidebarMenu>
       </SidebarContent>
     </SidebarGroup>
